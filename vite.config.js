60d61fce5d4f2a94c42090c451784b1325591f01
tanel5 <57736087+tanel5@users.noreply.github.com>
2024-09-16 21:11:34 +0200

    commit 3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "business-app-modern-UI",
   plugins: [react()],
   base: '/business-app-modern-UI/',
 })
